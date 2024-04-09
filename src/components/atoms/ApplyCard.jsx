fac81f350d0b114bbd1fc53bc8637877a9aead17
rony <rniumo@gmail.com>
2024-04-09 20:00:44 +0600

    apply

--- src/components/atoms/ApplyCard.jsx
+++ src/components/atoms/ApplyCard.jsx
@@ -1,9 +1,34 @@
-import React from 'react';
+import { useState } from 'react';
 import {
     Box, Typography, Button
 } from '@mui/material';
+import axios from 'axios';
+import * as urls from '../../utils/api_urls';
+import { message } from 'antd';
+import { loadInfo, setLoaded } from '../../redux/studentStoreReducer';
+import { useDispatch } from 'react-redux';
 
-const PendingCard = ({onApply}) => {
+
+const PendingCard = () => {
+    const dispatch = useDispatch();
+    const [isApplying, setIsApplying] = useState(false);
+    const apply = async () => {
+        setIsApplying(true);
+        try {
+            await axios.get(urls.applyClearanceUrl);
+            message.success('Applied For Clearance');
+            let res = await axios.get(urls.studentinfoUrl);
+            dispatch(loadInfo(res.data.info));
+        } catch (error) {
+            let error_msg = error?.response?.data?.details;
+            if (error_msg === undefined) {
+                error_msg = error.message;
+            }
+            message.error(error_msg);
+            dispatch(setLoaded(true));
+        }
+        setIsApplying(false);
+    }
     return (
         <Box sx={{ display: 'flex', mt: 5, flexDirection: 'column' }} alignItems="center" justifyContent="center">
             <Box sx={{ display: { xs: 'none', md: 'block' } }}>
@@ -13,7 +38,11 @@ const PendingCard = ({onApply}) => {
                 <img src="./static/images/apply.svg" width="150px" alt="" />
             </Box>
             <Typography variant="h5" fontSize={{ xs: '1rem', md: '1.7rem' }} textAlign="center">Great!!! Now You Can Apply For Clearance</Typography>
-            <Button variant='contained' sx={{ mt: 3, px: 5, py: 1, borderRadius: '180px' }}>Apply Now</Button>
+            {
+                isApplying ?
+                    <Button variant='contained' disabled sx={{ mt: 3, px: 5, py: 1, borderRadius: '180px' }}>Apply Now</Button>
+                    : <Button variant='contained' onClick={apply} sx={{ mt: 3, px: 5, py: 1, borderRadius: '180px' }}>Apply Now</Button>
+            }
         </Box>
     )
 }
