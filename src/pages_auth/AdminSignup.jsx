55459557296f5ebce04ba2fd6c45eae22b5d94d7
rony <rniumo@gmail.com>
2024-04-16 14:52:32 +0600

    signup

--- src/pages_auth/AdminSignup.jsx
+++ src/pages_auth/AdminSignup.jsx
@@ -3,27 +3,96 @@ import {
     Box, Typography, Stack, Button, TextField, Grid
 } from '@mui/material';
 import { message, Spin } from 'antd';
+import PictureInput from '../components/atoms/PictureInput'
 import axios from 'axios';
 import * as urls from '../utils/api_urls';
+import { useNavigate } from 'react-router-dom';
 
 
 const AdminSignup = () => {
+    const [token, setToken] = useState('');
     const [tokenValidated, setTokenValidated] = useState(false);
     const [isTokenValid, setIsTokenValid] = useState(false);
-    const handleSubmit = event => {
+    const [formData, setFormData] = useState(
+        {
+            first_name: '',
+            last_name: '',
+            password: '',
+        }
+    );
+    const [fileInfo, setFileInfo] = useState('No File Selected');
+    const [rePass, setRePass] = useState(null);
+    const [profilePhoto, setprofilePhoto] = useState(null);
+    const navigate = useNavigate();
+
+    const handleSubmit = async event => {
         event.preventDefault()
-        console.log("submitted");
+        if (formData.password !== rePass) {
+            message.error("Passwords doesn't matches", 5)
+            return;
+        }
+        if (profilePhoto === null) {
+            message.error("Profile photo not selected", 5)
+            return;
+        }
+        const postData = new FormData();
+        for (const key in formData) {
+            if (key !== 'last_name' && formData[key].length == 0) {
+                message.warning(`${key} cannot be empty`);
+                return;
+            }
+            postData.append(key, formData[key]);
+        }
+        postData.append('profilePhoto', profilePhoto);
+        postData.append('tokenid', token);
+        try {
+            await axios.post(urls.adminSignupApiUrl, postData);
+            message.success("Signup complete", 5)
+            setTimeout(() => {
+                navigate('/');
+            }, 1000)
+        } catch (error) {
+            let info = error?.response?.data?.details
+            if (info === undefined) {
+                info = error.message;
+            }
+            message.error(info, 5)
+        }
+    }
+
+    const handleChange = e => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value
+        })
     }
 
     async function validateToken(tokenid) {
-        let params = {tokenid: tokenid}
+        let params = { tokenid: tokenid }
         try {
-            await axios.get(urls.validateTokenUrl, {params});
+            await axios.get(urls.validateTokenUrl, { params });
             setIsTokenValid(true);
+            setToken(tokenid);
         } catch (error) {
             setIsTokenValid(false);
         }
         setTokenValidated(true);
+    }
+
+    const handleFileChange = e => {
+        let file = e.target.files[0];
+        let filesize = (file.size / 1024).toFixed(1);
+        if (file.type !== 'image/png' & file.type !== 'image/jpeg') {
+            message.error("Invalid File Type", 4);
+            return;
+        }
+        if (filesize > 1000) {
+            message.error("File too large. Must be less than 1MB", 5);
+            return;
+        }
+        let info = `Image selected. Size: ${filesize}KB`
+        setFileInfo(info);
+        setprofilePhoto(file);
     }
 
     useEffect(() => {
@@ -58,20 +127,43 @@ const AdminSignup = () => {
                                 <form action="" onSubmit={handleSubmit} >
                                     <Grid container spacing={3} sx={{ mt: 4 }}>
                                         <Grid item xs={12} md={6}>
-                                            <TextField label="First Name" variant='outlined' required fullWidth />
+                                            <TextField label="First Name" name='first_name' onChange={handleChange} variant='outlined' required fullWidth />
                                         </Grid>
                                         <Grid item xs={12} md={6}>
-                                            <TextField label="Last Name" variant='outlined' fullWidth />
+                                            <TextField label="Last Name" name='last_name' onChange={handleChange} variant='outlined' fullWidth />
                                         </Grid>
                                         <Grid item xs={12}>
-                                            <TextField label="Passoword" type="password" variant='outlined' required fullWidth />
+                                            <TextField label="Passoword" name='password' onChange={handleChange} type="password" variant='outlined' required fullWidth />
                                         </Grid>
                                         <Grid item xs={12}>
-                                            <TextField label="Retype Passoword" type="password" variant='outlined' required fullWidth />
+                                            {
+                                                (rePass !== null) & (rePass !== formData.password) ?
+                                                    <TextField
+                                                        label="Retype Passoword"
+                                                        error
+                                                        type="password"
+                                                        onChange={e => setRePass(e.target.value)}
+                                                        variant='outlined'
+                                                        required fullWidth
+                                                    /> :
+                                                    <TextField
+                                                        label="Retype Passoword"
+                                                        type="password"
+                                                        onChange={e => setRePass(e.target.value)}
+                                                        variant='outlined'
+                                                        required fullWidth
+                                                    />
+                                            }
+                                        </Grid>
+                                        <Grid item xs={12}>
+                                            <Stack direction={{ xs: 'column', md: 'row' }} alignItems="center" spacing={1}>
+                                                <PictureInput onChange={handleFileChange} />
+                                                <Typography variant='body2' color="text.secondary">{fileInfo}</Typography>
+                                            </Stack>
                                         </Grid>
                                         <Grid item xs={12}>
                                             <Box display="flex" justifyContent="flex-end">
-                                                <Button sx={{ px: 5 }} variant='contained'>Signup</Button>
+                                                <Button type='submit' sx={{ px: 5 }} variant='contained'>Signup</Button>
                                             </Box>
                                         </Grid>
                                     </Grid>
