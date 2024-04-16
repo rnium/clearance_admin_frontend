326736c4632320e20f37c1be664567214cfa94d8
rony <rniumo@gmail.com>
2024-04-16 20:01:20 +0600

    fixes

--- src/pages_auth/LoginRequired.jsx
+++ src/pages_auth/LoginRequired.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import React from 'react';
+import {
+  TextField, Button, Stack, Typography, Fade, Alert
+} from '@mui/material';
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+
+import { message } from 'antd'
+import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 const LoginRequired = () => {
   return (
-    <div>LoginRequired</div>
+    <Fade in={true}>
+      <div>
+        <Alert severity="info">You need to login to view this page</Alert>
+        <Stack direction="row" sx={{ mt: 2 }} justifyContent="left" alignItems="center">
+          <ArrowBackIosIcon fontSize='small' color='primary' />
+          <Link to="/">
+            <Typography color="primary" variant='body1' >Login</Typography>
+          </Link>
+        </Stack>
+      </div>
+    </Fade>
   )
 }
 
