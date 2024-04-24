ecbadcded75368258f8cb0d5e83629406bfc5911
rony <rniumo@gmail.com>
2024-04-24 15:12:27 +0600

    edit icon

--- src/components/molecules/StudentProfile.jsx
+++ src/components/molecules/StudentProfile.jsx
@@ -1,19 +1,30 @@
 import React from 'react';
 import {
-    Box, Typography, Paper, Avatar, Stack, Chip, Fade
+    Box, Typography, Paper, Avatar, Stack, Chip, Fade, IconButton
 } from '@mui/material';
 import VerifiedIcon from '@mui/icons-material/Verified';
 import HourglassBottomIcon from '@mui/icons-material/HourglassBottom';
 import * as urls from '../../utils/api_urls';
-
+import NoAccountsIcon from '@mui/icons-material/NoAccounts';
+import { useSelector } from 'react-redux';
+import ManageAccountsIcon from '@mui/icons-material/ManageAccounts';
 
 
 const StudentProfile = ({ student, onProgressClick }) => {
+    const usertype = useSelector(state => state.account.userinfo.user_type);
     return (
         <Fade in={true}>
             <Paper style={{ overflow: 'hidden' }}>
-                <Box>
-                    <Stack direction="row" sx={{ p: 2 }} justifyContent="center" >
+                <Box sx={{ position: 'relative' }}>
+                    {
+                        usertype === 'principal' || usertype === 'academic' || usertype === 'cashier' ?
+                            <Box sx={{ position: 'absolute', p: 1, right: 0, bottom: 0 }}>
+                                <IconButton >
+                                    <ManageAccountsIcon />
+                                </IconButton>
+                            </Box> : null
+                    }
+                    <Stack direction="row" sx={{ px: 2, py: 3 }} justifyContent="center" >
                         <Avatar
                             alt="User Name"
                             src={urls.baseUrl + student.avatar_url}
@@ -27,24 +38,39 @@ const StudentProfile = ({ student, onProgressClick }) => {
                                 {student.registration}
                             </Typography>
                             {
-                                student.progress < 100 ?
+                                student.hall ?
+                                    <Typography variant='h6' color="primary" sx={{ fontSize: '0.8rem', mb: 0.5 }}>{student.hall}</Typography> :
+                                    <Typography variant='h6' color="text.secondary" sx={{ fontSize: '0.8rem', mb: 0.5 }}>Non-Resident</Typography>
+
+                            }
+                            {
+                                student.is_approved ?
+                                    student.progress < 100 ?
+                                        <Chip
+                                            icon={<HourglassBottomIcon />}
+                                            sx={{ px: 1 }}
+                                            variant="outlined"
+                                            label={`${student.progress}% Cleared`}
+                                            onClick={() => onProgressClick(student.registration)}
+                                            size='small'
+                                            color="secondary"
+                                        /> :
+                                        <Chip
+                                            icon={<VerifiedIcon />}
+                                            sx={{ px: 1 }}
+                                            variant="contained"
+                                            label={`${student.progress}% Cleared`}
+                                            onClick={() => onProgressClick(student.registration)}
+                                            size='small'
+                                            color="success"
+                                        /> :
                                     <Chip
-                                        icon={<HourglassBottomIcon />}
+                                        icon={<NoAccountsIcon />}
                                         sx={{ px: 1 }}
                                         variant="outlined"
-                                        label={`${student.progress}% Cleared`}
-                                        onClick={() => onProgressClick(student.registration)}
+                                        label="Not Approved"
                                         size='small'
-                                        color="secondary"
-                                    /> :
-                                    <Chip
-                                        icon={<VerifiedIcon />}
-                                        sx={{ px: 1 }}
-                                        variant="outlined"
-                                        label={`${student.progress}% Cleared`}
-                                        onClick={() => onProgressClick(student.registration)}
-                                        size='small'
-                                        color="primary"
+                                        color="error"
                                     />
                             }
                         </Stack>
